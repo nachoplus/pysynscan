0f4241c6aaf3f64380115678bb4a824f320faf6e
Nacho Mas <mas.ignacio@gmail.com>
2020-08-03 09:16:19 +0200

    Updated documentation

--- examples/goto.py
+++
@@ -1,5 +0,0 @@
-import synscan.synscanMotors as synscanMotors
-
-smc=synscanMotors.synscanMotors()
-smc.goto(0,45)
-smc.goto(0,0)
--- synscan/__init__.py
+++
@@ -1,2 +0,0 @@
-name = 'synscan'
-__all__ = ['synscanMotors', 'synscanComm']
